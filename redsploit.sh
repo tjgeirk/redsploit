04e9c52cf840ae5382b332692cf466d5f06a22db
Timothy Geirk <71552349+tjgeirk@users.noreply.github.com>
2021-09-04 21:39:28 -0700

    Update redsploit.sh

--- redsploit.sh
+++ redsploit.sh
@@ -2,10 +2,16 @@ metasploit(){
     curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
     chmod 755 msfinstall && \
     ./msfinstall
-}
+};
+
 kalibrew(){
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &&
     git clone https://github.com/tjgeirk/kalibrew && bash kalibrew/kalibrew.sh
-}
-metasploit
-kalibrew
+};
+
+byob(){
+    git clone https://github.com/malwaredllc/byob &&
+    cd byob/web-gui && sh startup.sh
+};
+
+metasploit; kalibrew; byob;
