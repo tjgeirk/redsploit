c72c7da7ce1fb69c90c2eee30827179e1d688019
Timothy Geirk <71552349+tjgeirk@users.noreply.github.com>
2021-09-05 20:33:09 -0700

    Update redsploit.sh

--- redsploit.sh
+++ redsploit.sh
@@ -2,16 +2,15 @@ metasploit(){
     curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
     chmod 755 msfinstall && \
     ./msfinstall
-};
-
+}; metasploit;
 kalibrew(){
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &&
-    git clone https://github.com/tjgeirk/kalibrew && bash kalibrew/kalibrew.sh
-};
-
+    list=("aircrack-ng" "amap" "apktool" "arp-scan" "binwalk" "capstone" "sphinxbase" "cowpatty" "crunch" "dc3dd" "ddrescue" "dex2jar" "dns2tcp" "dnsmap" "dnstracer" "dos2unix" "ettercap" "fcrackzip" "foremost" "fragroute" "hping" "httptunnel" "hydra" "john-the-ripper" "libbtbb" "libewf" "libfreefare" "libmicrohttpd" "libnfc" "lynis" "AssetCacheManagerUtil" "mfcuk" "mitmproxy" "msgpack" "ncrack" "nikto" "nmap" "p0f" "pixz" "proxychains-ng" "pwnat" "qemu" "reaver" "rtl-sdr" "rtpbreak" "sipp" "skipfish" "sleuthkit" "slowhttptest" "smali" "sqlmap" "ssdeep" "sslscan" "thc-pptp-bruter" "theharvester" "truecrack" "valgrind" "volatility" "wine" "winexe" "wireshark" "yara")
+    for item in "${list[@]}"
+    do brew install $item
+    done
+}; kalibrew;
 byob(){
     git clone https://github.com/malwaredllc/byob &&
     cd byob/web-gui && sh startup.sh
-};
-
-metasploit; kalibrew; byob;
+}; byob;
