b2d2b8e925084f26673e9183ce768003d584b9c4
Foo Bar <tjgeirk@users.noreply.github.com>
2021-11-08 03:08:28 -0800

    Update

--- redsploit.sh
+++ redsploit.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 #
-curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && chmod 755 msfinstall && ./msfinstall
+git clone https://github.com/malwaredllc/byob;
 
-/bin/bash -c “$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)”
+git clone https://github.com/htr-tech/nexphisher;
+
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && chmod 755 msfinstall && ./msfinstall
 
 list=("aircrack-ng" "amap" "apktool" "arp-scan" "binwalk" "capstone" "sphinxbase" "cowpatty" "crunch" "dc3dd" "ddrescue" "dex2jar" "dns2tcp" "dnsmap" "dnstracer" "dos2unix" "ettercap" "fcrackzip" "foremost" "fragroute" "hping" "httptunnel" "hydra" "john-the-ripper" "libbtbb" "libewf" "libfreefare" "libmicrohttpd" "libnfc" "lynis" "AssetCacheManagerUtil" "mfcuk" "mitmproxy" "msgpack" "ncrack" "nikto" "nmap" "p0f" "pixz" "proxychains-ng" "pwnat" "qemu" "reaver" "rtl-sdr" "rtpbreak" "sipp" "skipfish" "sleuthkit" "slowhttptest" "smali" "sqlmap" "ssdeep" "sslscan" "thc-pptp-bruter" "theharvester" "truecrack" "valgrind" "volatility" "wine" "winexe" "wireshark" "yara")
 
-for item in "${list[@]}" do;
+if [ "$OSTYPE"=="darwin" ]; then
+  curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh && \
+  for item in "${list[@]}" do;
      brew install $item
-done
+  done
+elif [ "$OSTYPE"=="gnu" ]; then
+  for item in "${list[@]}" do;
+     apt-get install $item
+  done
+else
+  echo "error"; exit;
+fi
 
-git clone https://github.com/malwaredllc/byob 
-git clone https://github.com/8L4NK/blackeye
