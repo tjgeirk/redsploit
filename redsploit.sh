7b67f54ccf7db59cb4f5491ca292b8306cf635d1
Timothy Geirk <71552349+tjgeirk@users.noreply.github.com>
2022-03-07 07:56:29 -0800

    Update redsploit.sh

--- redsploit.sh
+++ redsploit.sh
@@ -1,23 +1,88 @@
 #!/bin/bash
 #
-git clone https://github.com/malwaredllc/byob;
-
-git clone https://github.com/htr-tech/nexphisher;
-
-curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && chmod 755 msfinstall && ./msfinstall
-
-list=("aircrack-ng" "amap" "apktool" "arp-scan" "binwalk" "capstone" "sphinxbase" "cowpatty" "crunch" "dc3dd" "ddrescue" "dex2jar" "dns2tcp" "dnsmap" "dnstracer" "dos2unix" "ettercap" "fcrackzip" "foremost" "fragroute" "hping" "httptunnel" "hydra" "john-the-ripper" "libbtbb" "libewf" "libfreefare" "libmicrohttpd" "libnfc" "lynis" "AssetCacheManagerUtil" "mfcuk" "mitmproxy" "msgpack" "ncrack" "nikto" "nmap" "p0f" "pixz" "proxychains-ng" "pwnat" "qemu" "reaver" "rtl-sdr" "rtpbreak" "sipp" "skipfish" "sleuthkit" "slowhttptest" "smali" "sqlmap" "ssdeep" "sslscan" "thc-pptp-bruter" "theharvester" "truecrack" "valgrind" "volatility" "wine" "winexe" "wireshark" "yara")
-
-if [ "$OSTYPE"=="darwin" ]; then
-  curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh && \
-  for item in "${list[@]}" do;
-     brew install $item
-  done
-elif [ "$OSTYPE"=="gnu" ]; then
-  for item in "${list[@]}" do;
-     apt-get install $item
-  done
+if [ brew ]; then
+    alias apt="brew"
 else
-  echo "error"; exit;
+    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh && \
+    alias apt="brew"
 fi
+apt update;
+apt upgrade;
+apt autoremove;
+apt install git curl ruby rbenv python3-pip python3-venv;
+git clone https://github.com/malwaredllc/byob;
+git clone https://github.com/htr-tech/nexphisher;
+git clone https://github.com/beefproject/beef
 
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > \
+msfinstall && chmod 755 msfinstall && ./msfinstall
+list=( \
+    "aircrack-ng" \
+    "amap" \
+    "apktool" \
+    "arp-scan" \
+    "binwalk" \
+    "capstone" \
+    "sphinxbase" \
+    "cowpatty" \
+    "crunch" \
+    "dc3dd" \
+    "ddrescue" \
+    "dex2jar" \
+    "dns2tcp" \
+    "dnsmap" \
+    "dnstracer" \
+    "dos2unix" \
+    "ettercap" \
+    "fcrackzip" \
+    "foremost" \
+    "fragroute" \
+    "hping" \
+    "httptunnel" \
+    "hydra" \
+    "john-the-ripper" \
+    "libbtbb" \
+    "libewf" \
+    "libfreefare" \
+    "libmicrohttpd" \
+    "libnfc" \
+    "lynis" \
+    "AssetCacheManagerUtil" \
+    "mfcuk" \
+    "mitmproxy" \
+    "metasploit" \
+    "msgpack" \
+    "ncrack" \
+    "netcat" \
+    "nikto" \
+    "nmap" \
+    "p0f" \
+    "pixz" \
+    "proxychains-ng" \
+    "pwnat" \
+    "qemu" \
+    "reaver" \
+    "rtl-sdr" \
+    "rtpbreak" \
+    "sipp" \
+    "skipfish" \
+    "sleuthkit" \
+    "slowhttptest" \
+    "smali" \
+    "sqlmap" \
+    "ssdeep" \
+    "sslscan" \
+    "thc-pptp-bruter" \
+    "theharvester" \
+    "tor" \
+    "torsocks" \
+    "truecrack" \
+    "utm" \
+    "valgrind" \
+    "volatility" \
+    "wine" \
+    "winexe" \
+    "wireguard-tools" \
+    "wireshark" \
+    "yara" \
+)
