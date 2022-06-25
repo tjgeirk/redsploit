4bf8e7b92adb63b3d8a37c1d1fbb480d2bb7d022
Timothy Geirk <71552349+tjgeirk@users.noreply.github.com>
2022-06-25 14:25:08 -0700

    Update redsploit.sh

--- redsploit.sh
+++ redsploit.sh
@@ -1,91 +1,19 @@
 #!/bin/bash
 #
-if [ brew ]; then
-    alias apt="brew"
-else
-    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh && \
-    alias apt="brew"
-fi
-apt update;
-apt upgrade;
-apt autoremove;
-apt install git curl ruby rbenv python3-pip python3-venv;
-git clone https://github.com/malwaredllc/byob;
-git clone https://github.com/htr-tech/nexphisher;
-git clone https://github.com/beefproject/beef
-
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > \
 msfinstall && chmod 755 msfinstall && ./msfinstall
-list=( \
-    "aircrack-ng" \
-    "amap" \
-    "apktool" \
-    "arp-scan" \
-    "binwalk" \
-    "capstone" \
-    "sphinxbase" \
-    "cowpatty" \
-    "crunch" \
-    "dc3dd" \
-    "ddrescue" \
-    "dex2jar" \
-    "dns2tcp" \
-    "dnsmap" \
-    "dnstracer" \
-    "dos2unix" \
-    "ettercap" \
-    "fcrackzip" \
-    "foremost" \
-    "fragroute" \
-    "hping" \
-    "httptunnel" \
-    "hydra" \
-    "john-the-ripper" \
-    "libbtbb" \
-    "libewf" \
-    "libfreefare" \
-    "libmicrohttpd" \
-    "libnfc" \
-    "lynis" \
-    "AssetCacheManagerUtil" \
-    "mfcuk" \
-    "mitmproxy" \
-    "metasploit" \
-    "msgpack" \
-    "ncrack" \
-    "netcat" \
-    "nikto" \
-    "nmap" \
-    "p0f" \
-    "pixz" \
-    "proxychains-ng" \
-    "pwnat" \
-    "qemu" \
-    "reaver" \
-    "rtl-sdr" \
-    "rtpbreak" \
-    "sipp" \
-    "skipfish" \
-    "sleuthkit" \
-    "slowhttptest" \
-    "smali" \
-    "sqlmap" \
-    "ssdeep" \
-    "sslscan" \
-    "thc-pptp-bruter" \
-    "theharvester" \
-    "tor" \
-    "torsocks" \
-    "truecrack" \
-    "utm" \
-    "valgrind" \
-    "volatility" \
-    "wine" \
-    "winexe" \
-    "wireguard-tools" \
-    "wireshark" \
-    "yara" \
-)
-for item in "${list[@]}" do;
-    apt install $item
-done
+git clone https://github.com/malwaredllc/byob
+git clone https://github.com/htr-tech/nexphisher
+git clone https://github.com/beefproject/beef
+git clone https://github.com/gkbrk/slowloris
+wget https://xsser.03c8.net/xsser/xsser_1.8.3_all.deb
+sudo dpkg -i xsser_1.8.3_all.deb git clone https://github.com/fwaeytens/dnsenum
+git clone https://github.com/nmap/nmap
+git clone https://github.com/t0thkr1s/revshellgen
+git clone https://github.com/IvanGlinkin/Fast-Google-Dorks-Scan
+git clone https://github.com/sqlmapproject/sqlmap
+git clone https://github.com/Ettercap/ettercap
+git clone https://github.com/bettercap/bettercap
+git clone https://github.com/hak5/sharkjack-payloads
+git clone https://github.com/hak5/bashbunny-payloads
+git clone https://github.com/hak5/usbrubberducky-payloads
